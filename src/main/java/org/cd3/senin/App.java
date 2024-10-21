9755707378f420810cb1f2e652644794b183eb87
kebabresing <holainc115@gmail.com>
2024-10-21 20:14:49 +0700

    ❤

---
+++ src/main/java/org/cd3/senin/App.java
@@ -0,0 +1,11 @@
+package org.cd3.senin;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class App {
+public static void main(String[] args) {
+String text = "Hello, Maven!";
+String reversedText = StringUtils.reverse(text);
+System.out.println(reversedText);
+}
+}
